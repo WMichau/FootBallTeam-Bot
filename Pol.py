0a1bca17024e1341f0687b057b1407c04aa56f9d
Michau <michau@miszormail.pl>
2021-03-23 17:07:54 +0100

    first

---
+++ Pol.py
@@ -0,0 +1,76 @@
+import os
+import discord
+from dotenv import load_dotenv
+import requests
+from requests import Session
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from datetime import date
+import schedule
+from discord import Webhook, RequestsWebhookAdapter
+import re
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+webhook = Webhook.partial(822464268565872722, 'WFSqF6N56gouE6b0MQ_mxUbvikroNVm7a-zvt6hc6ZXyHo8fuPBHWTPHo9ApBLrL_91B', adapter=RequestsWebhookAdapter())
+
+def login():
+    browser.get('https://footballteam.pl')
+    browser.find_element_by_xpath('//*[@id="buttons"]/div/button[1]').click()
+    browser.find_element_by_xpath('//*[@id="modal-login"]/div/div/div[1]/div[1]').click()
+    username = browser.find_element_by_xpath('//*[@id="modal-login"]/div/div/div[2]/div/form/div[1]/input')
+    password = browser.find_element_by_xpath('//*[@id="modal-login"]/div/div/div[2]/div/form/div[2]/input')
+    username.send_keys('granat29@vp.pl')
+    password.send_keys('qwerty123')
+    browser.find_element_by_xpath('//*[@id="btn-login"]').click()
+
+def logout():
+    profile = browser.find_element_by_xpath('/html/body/div[1]/div[1]/header/div[1]/div[2]')
+    ActionChains(browser).move_to_element(profile).perform()
+    browser.find_element_by_xpath('/html/body/div[1]/div[1]/header/div[1]/div[2]/div[1]/ul/li[5]/a').click()
+    
+def getTask():
+    browser.get('https://pl.footballteamgame.com/dashboard')
+    browser.refresh()
+    task = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/main/div/div/div/div[3]/div[2]/div[1]/div/div[2]/p")))
+    task_formated = task.get_attribute("textContent")
+    webhook.send("Zadanie na " + date.today().strftime("%d/%m/%Y") + " : " + task_formated.strip())
+
+def clubTraining():
+    webhook.send("@everyone Trening klubowy!")
+    
+def getMatch():
+    browser.get('https://pl.footballteamgame.com/dashboard')
+    browser.implicitly_wait(5)
+    match = browser.find_element_by_xpath('/html/body/div[1]/div[1]/main/div/div/div/div[5]/div[2]/div[1]/div/div[1]/div/div[2]').get_attribute("textContent")
+    match = match.strip()
+    match = match[12:]
+    match = f'"{match}"'
+    schedule.every().day.at(match).do(matchNotification)
+
+def matchNotification():
+    webhook.send("Mecz ligowy")
+
+schedule.every().day.at("16:36").do(login)
+schedule.every().day.at("12:23").do(getTask)
+schedule.every().day.at("16:37").do(getMatch)
+schedule.every().day.at("12:34").do(clubTraining)
+schedule.every().day.at("12:35").do(logout)
+
+@client.event
+async def on_ready():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+client.run(TOKEN)
+
+
