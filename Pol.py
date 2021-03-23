68c8285eb69a90b533cd77e9e0f430914a6cb836
Michau <michau@miszormail.pl>
2021-03-23 17:57:27 +0100

    first

--- Pol.py
+++ Pol.py
@@ -14,21 +14,21 @@ import schedule
 from discord import Webhook, RequestsWebhookAdapter
 import re
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver' 
+GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
-browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 webhook = Webhook.partial(822464268565872722, 'WFSqF6N56gouE6b0MQ_mxUbvikroNVm7a-zvt6hc6ZXyHo8fuPBHWTPHo9ApBLrL_91B', adapter=RequestsWebhookAdapter())
 
